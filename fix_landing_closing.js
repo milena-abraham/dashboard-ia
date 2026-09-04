7207e9268c3d76ef36fe5798b4dbc3043c0eb02c
ivysallover <tadeomunoz@hotmail.com.ar>
2026-09-04 18:24:09 -0300

    chore: eliminar scripts de parche temporales y directorios obsoletos

--- fix_landing_closing.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('frontend/src/app/page.tsx', 'utf8');
-
-content = content.replace(/<\/p>\n            <\/div>/g, '<\/p>\n            <\/div>\n          <\/StaggerItem>');
-
-// Make sure StaggerContainer is closed
-content = content.replace(/<\/StaggerItem>\n\n          <\/div>/g, '<\/StaggerItem>\n\n          <\/div>\n          <\/StaggerContainer>');
-
-fs.writeFileSync('frontend/src/app/page.tsx', content, 'utf8');
